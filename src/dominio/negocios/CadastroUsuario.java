23f62a5ec924f6db5df9766fe16074f08b1afacd
Wellington Viana <osmcanal@gmail.com>
2024-08-15 04:40:54 -0300

    Novo corpo e novas modificações do Projeto

--- src/dominio/negocios/CadastroUsuario.java
+++ src/dominio/negocios/CadastroUsuario.java
@@ -4,52 +4,58 @@ import dominio.dados.RepositorioUsuarioList;
 import dominio.dados.interfaces.IRepositorioGeneric;
 import dominio.exceptions.ElementoJaExisteException;
 import dominio.exceptions.ElementoNaoExisteException;
-import dominio.negocios.beans.Conteudo;
-import dominio.negocios.beans.Produtora;
+import dominio.exceptions.ElementoNullException;
+import dominio.exceptions.MesmoElementoException;
 import dominio.negocios.beans.Usuario;
 
-public class CadastroUsuario {
+public class CadastroUsuario  {
+    private static CadastroUsuario instancia;
+    private final IRepositorioGeneric<Usuario> repositorio;
 
-    private IRepositorioGeneric<Usuario> repositorio;
-
-    public CadastroUsuario(IRepositorioGeneric<Usuario> repositorio) {
-        this.repositorio = repositorio;
+    private CadastroUsuario() {
+        this.repositorio = RepositorioUsuarioList.getInstance();
     }
 
-    public void cadastrar(Usuario u){
+    public static CadastroUsuario getInstance() {
+        if(instancia == null) {
+            instancia = new CadastroUsuario();
+
+        }
+        return instancia;
+    }
+
+    public void cadastrarUsuario(Usuario u) throws ElementoJaExisteException, ElementoNullException {
         if(u != null) {
             if (!repositorio.existe(u.getEmail())) {
                 this.repositorio.cadastrar(u);
+            } else {
+                throw new ElementoJaExisteException();
             }
         } else {
             throw new ElementoNullException();
         }
-
     }
 
-    public void remover(String email) throws ElementoNaoExisteException {
+    public void removerUsuario(String email) throws ElementoNaoExisteException {
         Usuario removido = this.repositorio.procurar(email);
         if(removido != null){
             this.repositorio.remover(email);
         }
-        else{
-            throw new ElementoNaoExisteException();
-        }
     }
 
-    public Usuario procurar(String email) throws ElementoNaoExisteException {
+    public Usuario procurarUsuario(String email) throws ElementoNaoExisteException {
         return this.repositorio.procurar(email);
     }
 
-    public int procurarIndice(String email) throws ElementoNaoExisteException{
+    public int procurarIndiceUsuario(String email) throws ElementoNaoExisteException{
         return this.repositorio.procurarIndice(email);
     }
 
-    public boolean existe(String email){
+    public boolean existeUsuario(String email){
         return this.repositorio.existe(email);
     }
 
-    public void atualizar(Usuario antigo, Usuario novo) throws ElementoNaoExisteException, ElementoJaExisteException {
+    public void atualizarUsuario(Usuario antigo, Usuario novo) throws ElementoNullException, MesmoElementoException, ElementoJaExisteException {
         if(!antigo.equals(novo)) {
             if(!this.repositorio.existe(novo.getEmail())) {
                 this.repositorio.atualizar(antigo, novo);
@@ -59,8 +65,6 @@ public class CadastroUsuario {
         else{throw new MesmoElementoException();}
     }
 
-    public void imprimirDados(String email) throws ElementoNaoExisteException{
-        this.repositorio.dadosString(email);
-    }
+
 
 }
--- src/dominio/negocios/beans/Produtora.java
+++ src/dominio/negocios/beans/Produtora.java
@@ -21,7 +21,6 @@ public class Produtora extends Usuario {
         this.produto = produto;
     }
 
-    //Getters e setters
 
 
     @Override
